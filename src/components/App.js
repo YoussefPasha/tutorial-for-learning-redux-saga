cab32bcdff7a85e7a6c805d092c7da8b8d28e926
YoussefSedekPasha <45726395+YoussefPasha@users.noreply.github.com>
2020-10-12 14:24:25 +0200

    done getting data from rest api using react , redux , redux saga

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,17 @@
 import React from "react";
 import { Component } from "react";
-
-// put some examples to learn about yield
-function* testing() {
-  while (true) {
-    yield 1;
-    yield 2;
-    yield 3;
-  }
-}
-
+import { connect } from "react-redux";
+import { getUsersRequest } from "../actions/users";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.props.getUsersRequest();
+  }
   render() {
-    // create iterator from testing
-    // to get all data from it
-    const iterator = testing();
-    // in last commit i dont have loop to yield data on it
-    // now my explain is on while loop
-    // output is diffrent from last commit now output is 1 & 2 & 3 &1
-    // but what is the explanition about this
-    // yield is a {{{{LOOP}}}}
-    // and when yield get last data 1 as a 2 iteration in loop
-    // it stops to check if it has 1
-    // so it is one and stops To understand it goto directory sagas and users.js
-    console.log(iterator.next());
-    console.log(iterator.next());
-    console.log(iterator.next());
-    console.log(iterator.next());
     return <div></div>;
   }
 }
 
-export default App;
+export default connect(null, {
+  getUsersRequest,
+})(App);
