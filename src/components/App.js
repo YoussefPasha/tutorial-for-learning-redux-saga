f26f53ef9a30cc8141a3b9703ac78fa67c834e32
YoussefSedekPasha <45726395+YoussefPasha@users.noreply.github.com>
2020-10-13 16:49:04 +0200

    creating ui for adding new user in list

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,20 @@ import { Component } from "react";
 import { connect } from "react-redux";
 import { getUsersRequest } from "../actions/users";
 import UsersList from "./UsersList";
+import NewUSerForm from "./NewUserForm";
 class App extends Component {
   constructor(props) {
     super(props);
     this.props.getUsersRequest();
   }
+  handleSubmit = ({ firstName, lastName }) => {
+    console.log(firstName, lastName);
+  };
   render() {
     const users = this.props.users;
     return (
       <div style={{ margin: "0 auto", padding: "20px", maxWidth: "600px" }}>
+        <NewUSerForm onSubmit={this.handleSubmit} />
         <UsersList users={users.items} />
       </div>
     );
